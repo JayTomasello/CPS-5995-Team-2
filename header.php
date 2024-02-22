be24b3e6279bcb2ac97f285ca6d7f0665eaf696d
bl4kaspekt <109128654+bl4kaspekt@users.noreply.github.com>
2024-02-22 18:01:21 -0500

    modified:   header.php

--- header.php
+++ header.php
@@ -4,11 +4,6 @@ include('dbconfig.php');
 // Test Cookie for development
 $_COOKIE = false;
 
-if ($_COOKIE != true) :
-?> <script>
-        window.location.href = "terms.php";
-    </script>
-<?php endif;
 ?>
 
 <!-- <script>
@@ -31,6 +26,22 @@ if ($_COOKIE != true) :
 </head>
 <!-- 80, 104, 148 -->
 <!-- (170, 159, 91) -->
+
+<div class="modal-dialog modal-fullscreen">
+    <div class="modal-content">
+        <div class="modal-header">
+            <h5 class="modal-title">NJLaw Digest</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+            <p>Modal body text goes here.</p>
+        </div>
+        <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary">Save changes</button>
+        </div>
+    </div>
+</div>
 
 <nav class="navbar navbar-expand-lg bg" style="background-color:white">
     <div class="container-fluid">
