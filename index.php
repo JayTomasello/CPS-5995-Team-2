e1085d050e481ba8ee6458fe134f2e0465ce94cf
bl4kaspekt <109128654+bl4kaspekt@users.noreply.github.com>
2024-02-23 12:33:55 -0500

    modified:   index.php

--- index.php
+++ index.php
@@ -7,12 +7,12 @@ include('header.php');
         <div class="row align-items-center m-3">
 
             <!-- Robo Lawyer Section -->
-            <div class="col" style="background-color:black">
+            <div class="col" style="background-color:rgb(80, 104, 148); border-color:white">
                 Hello World
             </div>
 
             <!-- Document Viewer -->
-            <div class="col" style="background-color:black">
+            <div class="col" style="background-color:rgb(80, 104, 148); border-color:white">
                 Hello World
             </div>
         </div>
