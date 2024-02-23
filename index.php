791ec8d97962325facc32efc6b9deefa575369b6
bl4kaspekt <109128654+bl4kaspekt@users.noreply.github.com>
2024-02-23 18:51:24 -0500

    modified:   index.php

--- index.php
+++ index.php
@@ -7,22 +7,22 @@ include('header.php');
     <div class="d-flex justify-content-evenly" style="height:88vh; background-color: rgb(170, 159, 91)">
 
         <!-- Robo Lawyer Section -->
-        <div class="col-4 h-auto d-inline-block my-4 rounded border-3" style="background-color:white">
-            <div class="m-1 h-100 rounded" style="background-color: rgb(80, 104, 148);">
-                <div class="d-inline-block my-4">
-                    <input class="input-group-text" placeholder="Ask our robot lawyer...">
-                    <img class="img m-3" src="./robot lawyer.png" style="width:100px;">
+        <div class="col-4 h-auto d-inline-block my-4 rounded-5 border-3" style="background-color:white">
+            <div class="h-100 m-1 rounded-5" style="background-color: rgb(80, 104, 148);">
+                <div class="d-inline-flex m-4">
+                    <input class="col-12 input-group-text rounded-5" placeholder="Ask our robot lawyer...">
+                    <img class="img mx-5" src="./robot lawyer.png" style="width:100px;">
                 </div>
-                <div>
-
+                <div class="m-4 p-3 h-75 rounded-5" style="background-color:white">
+                    Hello World
                 </div>
             </div>
 
         </div>
 
         <!-- Document Viewer -->
-        <div class="col-7 h-auto d-inline-block my-4 rounded border-3" style="background-color:white">
-            <div class="m-1 rounded" style="background-color: rgb(80, 104, 148);">
+        <div class="col-7 h-auto d-inline-block my-4 rounded-5 border-3" style="background-color:white">
+            <div class="h-100 m-1 rounded-5" style="background-color: rgb(80, 104, 148);">
                 <div class="">Hello</div>
                 <div class="">World</div>
             </div>
