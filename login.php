7a598c52d0aa914a22a735fa26e6f3b2b1e2fba1
bl4kaspekt <109128654+bl4kaspekt@users.noreply.github.com>
2024-02-24 14:41:42 -0500

    modified:   login.php

--- login.php
+++ login.php
@@ -9,7 +9,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </head>
 
-<body class="container justify-content-center" style="background-image: url(./Courthouse.jpg); background-size:cover; background-position:center -100px">
+<body class="container align-middle justify-content-center" style="background-image: url(./Courthouse.jpg); background-size:cover; background-position:center -100px">
     <h3 class="text-center" style="margin-top: 50px;">By Xavier Amparo, Matthew Fernandez, Julio Rodriguez, and Joseph Tomasello</h3>
     <form class="w-50 text-center content-center m-3" action="logintest.php" method="POST">
         <h1>Law Digest 4 New Jersey</h1>
