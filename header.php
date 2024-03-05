9d49e81dca5e1d2d008a29efecec27f73f589e7b
JayTomasello <JayThomas3.14159@gmail.com>
2024-03-05 10:36:39 -0500

    Update header.php

--- header.php
+++ header.php
@@ -12,11 +12,17 @@ session_start();
     <link rel="icon" href="NJLawDigest Logo.png">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <style>
+        .dropdown-menu-scrollable {
+            max-height: 200px;
+            overflow-y: auto;
+        }
+    </style>
 </head>
 
 <body>
     <?php
-    if ((!isset($_COOKIE['signedIn'])) && ((!isset($_SESSION['agree'])) || ($_SESSION['agree'] == FALSE))) {
+    if ((!isset($_COOKIE['email'])) && ((!isset($_SESSION['agree'])) || ($_SESSION['agree'] == FALSE))) {
         echo ('<div class="z-2 position-absolute w-100 h-100 opacity-50 bg-black"></div>');
     }
     ?>
@@ -29,9 +35,11 @@ session_start();
             <label class="navbar-brand fs-1 text-dark me-4" style="font-family: Georgia, 'Times New Roman', Times, serif">Law Digest 4 New Jersey</label>
 
             <div class="btn-group m-3">
-                <button type="button" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown">Search Laws by Category<span class="caret"></span>
-                    <ul class="dropdown-menu scrollable-menu" role="menu">
-                        <li><a class="dropdown-item" href="#">Abortion</a></li>
+                <button type="button" class="btn btn-secondary dropdown-toggle" id="dropdownMenuOffset1" data-bs-toggle="dropdown" aria-expanded="false" data-bs-offset="10,20">
+                    Search Laws by Subject
+                </button>
+                <ul class="dropdown-menu dropdown-menu-scrollable">
+                <li><a class="dropdown-item" href="#">Abortion</a></li>
                         <li><a class="dropdown-item" href="#">Agriculture</a></li>
                         <li><a class="dropdown-item" href="#">Alcohol</a></li>
                         <li><a class="dropdown-item" href="#">Animals</a></li>
@@ -120,18 +128,27 @@ session_start();
                         <li><a class="dropdown-item" href="#">Veterans and Military</a></li>
                         <li><a class="dropdown-item" href="#">Wills, Trusts, and Estates</a></li>
                         <li><a class="dropdown-item" href="#">Women</a></li>
-                    </ul>
-                </button>
+                </ul>
             </div>
 
             <div class="btn-group m-3">
-                <button type="button" class="btn btn-secondary dropdown-toggle" id="dropdownMenuOffset" data-bs-toggle="dropdown" aria-expanded="false" data-bs-offset="10,20">
+                <button type="button" class="btn btn-secondary dropdown-toggle" id="dropdownMenuOffset2" data-bs-toggle="dropdown" aria-expanded="false" data-bs-offset="10,20">
                     Search Laws by Legislative Session
                 </button>
                 <ul class="dropdown-menu">
-                    <li><a class="dropdown-item" href="#">Action</a></li>
-                    <li><a class="dropdown-item" href="#">Another action</a></li>
-                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                <li><a class="dropdown-item" href="#">2024-2025 Session</a></li>
+                    <li><a class="dropdown-item" href="#">2022-2023 Session</a></li>
+                    <li><a class="dropdown-item" href="#">2020-2021 Session</a></li>
+                    <li><a class="dropdown-item" href="#">2018-2019 Session</a></li>
+                    <li><a class="dropdown-item" href="#">2016-2017 Session</a></li>
+                    <li><a class="dropdown-item" href="#">2014-2015 Session</a></li>
+                    <li><a class="dropdown-item" href="#">2012-2013 Session</a></li>
+                    <li><a class="dropdown-item" href="#">2010-2011 Session</a></li>
+                    <li><a class="dropdown-item" href="#">2008-2009 Session</a></li>
+                    <li><a class="dropdown-item" href="#">2006-2007 Session</a></li>
+                    <li><a class="dropdown-item" href="#">2004-2005 Session</a></li>
+                    <li><a class="dropdown-item" href="#">2002-2003 Session</a></li>
+                    <li><a class="dropdown-item" href="#">2000-2001 Session</a></li>
                 </ul>
             </div>
 
@@ -140,25 +157,24 @@ session_start();
         <div class="container-fluid justify-content-end">
             <?php
             if (isset($_COOKIE['email'])) {
-                echo ('<label class="navbar-brand fs-4 text-dark me-4" style="font-family: Georgia, ' . 'Times New Roman' . ' , Times, serif">Welcome, ' . $_COOKIE['email'] . '.</label>
-                <img src="./User Icon.png" class="img" width="50px" alt="...">
-                <a type="button" class="btn btn-secondary mx-4" href="logout.php">Logout</a>
-                <button class="btn-group m-2"> 
-                    <img src="./settings.png" class="img" width="50px" alt="...">
-                    <label class="fs-1">Settings</label>                
-                </button>
-                ');
+                echo ('<div class="btn-group m-3">
+                            <button type="button" class="btn btn-secondary dropdown-toggle" id="userMenu" data-bs-toggle="dropdown" aria-expanded="false">
+                                Welcome, ' . $_COOKIE['email'] . '
+                            </button>
+                            <ul class="dropdown-menu" aria-labelledby="userMenu">
+                                <li><a class="dropdown-item" href="logout.php">Logout</a></li>
+                                <li><a class="dropdown-item" href="#">Settings</a></li>
+                            </ul>
+                        </div>');
             } else {
                 echo ('<label class="navbar-brand fs-4 text-dark me-4" style="font-family: Georgia, ' . 'Times New Roman' . ' , Times, serif">Welcome, Guest.</label>
                     <img src="./User Icon.png" class="img" width="50px" alt="...">
                     <a type="button" class="btn btn-secondary mx-4" href="login.php">Login</a>
                 ');
             }
-
             ?>
-
-
-
-
         </div>
     </nav>
+</body>
+
+</html>
